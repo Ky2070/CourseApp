4a68bf54b5778d09ede65e563399bfafce01b7fd
unknown <2051052070ky@ou.edu.vn>
2025-02-19 11:32:02 +0700

    add stats admin-django

--- courseapis/courses/admin.py
+++ courseapis/courses/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
+from django.db.models import Count
+from django.template.response import TemplateResponse
+
 from courses.models import Category, Course, Lesson, Tag
 from django.utils.html import mark_safe
 from django import forms
@@ -26,8 +29,14 @@ class LessonAmin(admin.ModelAdmin):
     list_filter = ['subject', 'created_at']
     search_fields = ['subject', 'course__subject']
     form = LessonForm
+class CourseAppAdminSite(admin.AdminSite):
+    site_header = 'Hệ thống khóa học trực tuyến'
+    def stats_view(self, request):
+        count = Course.objects.filter(activate=True).count()
+        stats = Course.objects.annotate(lesson_count=Count('lesson__id')).values('id', 'subject', 'lesson_count')
 
-admin.site.register(Category)
-admin.site.register(Course, CourseAdmin)
-admin.site.register(Lesson, LessonAmin)
-admin.site.register(Tag)
+admin_site = CourseAppAdminSite(name='Admin')
+admin_site.register(Category)
+admin_site.register(Course, CourseAdmin)
+admin_site.register(Lesson, LessonAmin)
+admin_site.register(Tag)
